5b426808a6baf72a9cd4c77f7081c7053d4b7506
Wisnu Agung <65849194+Artheneus@users.noreply.github.com>
2024-06-03 08:40:13 +0800

    server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,9 @@ app.get('/order/success', async (req, res) => {
   const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
   const customer = await stripe.customers.retrieve(session.customer);
 
-
+res.send(
+      `<html><body><h1>Thanks for your order, ${customer.name}!</h1></body></html>`
+    );
 });
   
   const session = await stripe.checkout.sessions.create({
