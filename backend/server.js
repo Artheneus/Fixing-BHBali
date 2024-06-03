626aded2185b59e8ffeac758afa1d7db74ca2b30
Wisnu Agung <65849194+Artheneus@users.noreply.github.com>
2024-06-03 08:21:35 +0800

    server.js

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.post("/checkout", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:5173/success",
-    cancel_url: "http://localhost:5173/cancel",
+    success_url: "https://fixing-bh-bali.vercel.app/success",
+    cancel_url: "https://fixing-bh-bali.vercel.app/cancel",
   });
 
   res.send(
